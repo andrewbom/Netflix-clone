ef716c7723f358b5646fd8ae71ec65c920c40b1d
andrewbom <59633536+andrewbom@users.noreply.github.com>
2020-07-29 01:49:21 +1000

    fixed the bugs

--- src/requests.js
+++ src/requests.js
@@ -2,8 +2,8 @@ const API_KEY = "034633e04352959cd4424c9d9718e3c2";
 
 const requests = {
   fetchTrending: `/trending/movie/day?api_key=${API_KEY}`,
-  fetchNetflixOriginals: `/trending/tv/day?api_key=${API_KEY}`,
-  fetchTopRated: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=true&include_video=false&page=1`,
+  fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`,
+  fetchTopRated: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_genres=18`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_genres=27`,
